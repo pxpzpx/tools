ceebcedfd81518f97f2e7c9953d6f151b882dc55
Jinhwa Joung <jjangun@gmail.com>
2015-12-26 00:07:10 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
-echo "Install gnu global"
-global_version="global-6.5"
-global_archive=$global_version".tar.gz"
-global_down_url="http://ftp.gnu.org/gnu/global/"$global_archive
-
-mkdir -p $HOME/bin/global
-wget $global_down_url -P $HOME
-tar xvzf $global_archive -C $HOME
-
-cd $HOME/$global_version
-./configure --prefix=$HOME/bin/global
-make
-make install
-
-echo "Remove install files"
-rm -rf $HOME/$global_version
-
-echo "export PATH+=:$HOME/bin/glboal/bin" >> ~/.bashrc
-echo "global install done"
-
 echo "Append config .vimrc"
 echo "source ~/tools/env/.vimrc" >> ~/.vimrc
 
@@ -43,5 +23,25 @@ rm -rf ~/.vim/colors/molokai
 
 echo "Install Vundle Plugins"
 vim -c :PluginInstall -c :qa
+
+echo "Install gnu global"
+global_version="global-6.5"
+global_archive=$global_version".tar.gz"
+global_down_url="http://ftp.gnu.org/gnu/global/"$global_archive
+
+mkdir -p $HOME/tools/bin/global
+wget $global_down_url -P $HOME
+tar xvzf $global_archive -C $HOME
+
+cd $HOME/$global_version
+./configure --prefix=$HOME/tools/bin/global
+make
+make install
+
+echo "Remove install files"
+rm -rf $HOME/$global_archive
+rm -rf $HOME/$global_version
+
+echo "global install done"
 
 echo "Completed!"
