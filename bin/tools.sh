ca3a21c3f60b7d7cabde01d7503495db293049fc
Jinhwa Joung <jjangun@gmail.com>
2016-02-18 10:40:00 +0900

    Update tools.sh for adding powerline

--- bin/tools.sh
+++ bin/tools.sh
@@ -15,6 +15,12 @@ source acd_func.sh
 alias cd..='cd ..'
 # }}}
 
+# powerline {{{
+if [ -f /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh ]; then
+    source /usr/local/lib/python2.7/dist-packages/powerline/bindings/bash/powerline.sh
+fi
+}}}
+
 # etc {{{
 export TERM='xterm-256color'
 # }}}
