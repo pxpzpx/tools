06b00aaf95b18274e236c6d1ef8ecfc359e73cbc
Jinhwa Joung <jjangun@gmail.com>
2016-08-17 10:54:07 +0900

    Update .ycm_extra_conf.py

--- env/.ycm_extra_conf.py
+++ env/.ycm_extra_conf.py
@@ -54,12 +54,8 @@ flags = [
 # language that the files to be compiled are written in. This is mostly
 # relevant for c++ headers.
 # For a C project, you would set this to 'c' instead of 'c++'.
-'-x',
-'c++',
-'-I',
-'inc',
-'-I',
-'../Inc'
+'-x', 'c++',
+'-I', '.'
 ]
 
 
