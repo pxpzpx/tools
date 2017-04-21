ff1c48d132572bb340a96c087106e726fa5f904a
Jinhwa Joung <jjangun@gmail.com>
2017-04-21 16:34:25 +0900

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -7,16 +7,6 @@ echo "source $HOME/tools/env/.vimrc" >> $HOME/.vimrc
 echo "Append config tools.sh"
 echo "source $HOME/tools/bin/tools.sh" >> $HOME/.bashrc
 
-echo "Downloading... solarized color scheme for vim"
-git clone git://github.com/altercation/vim-colors-solarized.git $HOME/.vim/colors/vim-colors-solarized
-cp $HOME/.vim/colors/vim-colors-solarized/colors/solarized.vim $HOME/.vim/colors/solarized.vim
-rm -rf $HOME/.vim/colors/vim-colors-solarized
-
-echo "Downloading... molokai color scheme for vim"
-git clone https://github.com/tomasr/molokai.git $HOME/.vim/colors/molokai
-cp $HOME/.vim/colors/molokai/colors/molokai.vim $HOME/.vim/colors/molokai.vim
-rm -rf $HOME/.vim/colors/molokai
-
 echo "Downloading... Vundle"
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
